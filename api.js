77caca1f4c6075b65b1790203df65edbcf8a5f02
Willi Raschkowski <willi@raschkowski.com>
2016-09-15 16:14:29 +0200

    Search stations only (not locations/streets)

--- api.js
+++ api.js
@@ -1,7 +1,27 @@
 let request = require('request');
 let config = require('./config.js');
 
-function getDepartures(id) {
+
+exports.getStations = function (query) {
+  return new Promise((resolve, reject) => {
+    // request( uri, options, callback );
+    request(
+      `${config.url}/location.name?input=${query}&accessId=${config.token}&format=json&type=S`,
+      {json: true},
+      // callback
+      (error, response, body) => {
+        // if we get an error or the response is not right (not 200),
+        // we call reject(), which calls 'func' in .catch(func)
+        if (error) return reject(error);
+        if (response.statusCode !== 200) return reject(new Error(body));
+        // otherwise we call resolve(), which calls 'func' in .then(func)
+        resolve(body);
+      });
+  });
+};
+
+
+exports.getDepartures = function (id) {
   return new Promise((resolve, reject) => {
     // request( uri, options, callback );
     request(
@@ -17,22 +37,4 @@ function getDepartures(id) {
         resolve(body);
       });
   });
-}
-
-function getStations(query) {
-  return new Promise((resolve, reject) => {
-    // request( uri, options, callback );
-    request(
-      `${config.url}/location.name?input=${query}&accessId=${config.token}&format=json`,
-      {json: true},
-      // callback
-      (error, response, body) => {
-        // if we get an error or the response is not right (not 200),
-        // we call reject(), which calls 'func' in .catch(func)
-        if (error) return reject(error);
-        if (response.statusCode !== 200) return reject(new Error(body));
-        // otherwise we call resolve(), which calls 'func' in .then(func)
-        resolve(body);
-      });
-  });
-}
+};
