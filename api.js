949a4df67544c8047144bee3eb62fd581ba92bfa
Willi Raschkowski <willi@raschkowski.com>
2016-09-18 01:07:16 +0200

    Add trip service

--- api.js
+++ api.js
@@ -1,40 +1,85 @@
 let request = require('request');
 let config = require('./config.js');
 
+var token = config.getToken();
+var baseUrl = config.url;
 
 exports.getStations = function (query) {
-  return new Promise((resolve, reject) => {
-    // request( uri, options, callback );
-    request(
-      `${config.url}/location.name?input=${query}&accessId=${config.token}&format=json&type=S`,
-      {json: true},
-      // callback
-      (error, response, body) => {
-        // if we get an error or the response is not right (not 200),
-        // we call reject(), which calls 'func' in .catch(func)
-        if (error) return reject(error);
-        if (response.statusCode !== 200) return reject(new Error(body));
-        // otherwise we call resolve(), which calls 'func' in .then(func)
-        resolve(body);
-      });
-  });
+  var serviceName = 'location.name'; // Name of the API service
+  var parameters = { // Parameters for the API
+    input: query,
+    accessId: token,
+    format: 'json',
+    type: 'S'
+  };
+  return callToAPI(serviceName, parameters); // Return the API call as promise
 };
-
 
 exports.getDepartures = function (id) {
+  var serviceName = 'departureBoard'; // Name of the API service
+  var parameters = {  // Parameters for the API
+    id: id,
+    accessId: token,
+    format: 'json'
+  };
+  return callToAPI(serviceName, parameters); // Return the API call as promise
+};
+
+exports.getTrip = function (fromId, toId) {
+  var serviceName = 'trip'; // Name of the API service
+  var parameters = {  // Parameters for the API
+    originId: fromId,
+    destId: toId,
+    accessId: token,
+    format: 'json'
+  };
+
+  return callToAPI(serviceName, parameters);
+};
+
+
+// Takes parameters as object and returns url parameter string, e.g.:
+// {a: 1, b: 2, c: 3} => a=1&b=1&c=1
+// see https://en.wikipedia.org/wiki/Query_string
+function urlParams(params, separator = '&') {
+  // Collect all key-value pairs in 'params'
+  // as 'key=value' strings
+  var pairs = [];
+  for (var key in params) {
+    pairs.push(key + '=' + params[key]);
+  }
+  // join and return all 'key=value' pairs joined with the separator
+  // i.e.: key1=value1&key2=value2&...
+  var queryString = pairs.join(separator);
+  return queryString;
+}
+
+
+// Return a promise to retrieve the requested data
+// To understand 'promises' in JS, check out:
+// https://spin.atomicobject.com/2016/02/16/how-javascript-promises-work/
+function callToAPI(serviceName, parameters) {
+  // Create a new promise
+  // If the data couldn't be retrieved, reject(error) is called
+  // If the data was retrieved, resolve(body) is called
   return new Promise((resolve, reject) => {
-    // request( uri, options, callback );
+    // The request is sent using a function called 'request'
+    //     request( uri, options, callback );
+    // (checkout https://github.com/request/request)
     request(
-      `${config.url}/departureBoard?id=${id}&accessId=${config.token}&format=json`,
+      // Url to send the request to
+      `${baseUrl}/${serviceName}?${urlParams(parameters)}`,
+      // The returned data is a json, make sure 'format=json' is in the params
       {json: true},
-      // callback
+      // Callback, the function called once the server (API) responded
       (error, response, body) => {
-        // if we get an error or the response is not right (not 200),
-        // we call reject(), which calls 'func' in .catch(func)
+        // If we get an error or the response isn't right (not 200 code),
+        // we call reject(), which calls 'func' in Promise.catch(func)
         if (error) return reject(error);
         if (response.statusCode !== 200) return reject(new Error(body));
-        // otherwise we call resolve(), which calls 'func' in .then(func)
+        // otherwise we call resolve(), which calls 'func' in Promise.then(func)
         resolve(body);
       });
   });
-};
+}
+exports.callToApi = callToAPI;
