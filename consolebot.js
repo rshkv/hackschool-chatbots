7d3995ac9e49e5e16a1307db1a56d5e9db4fc231
HendrikSchmidt <hendriknas@gmail.com>
2016-09-18 03:19:03 +0200

    bot understands different queries

--- consolebot.js
+++ consolebot.js
@@ -1,6 +1,5 @@
 var builder = require('botbuilder');
 var api = require('./api.js');
-var restify = require('restify');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
@@ -8,58 +7,72 @@ var intents = new builder.IntentDialog();
 
 console.log('What do you want to do?');
 
-bot.dialog('/', intents);
-
-intents.matches(/^departures/i, [
-    function (session) {
-        session.beginDialog('/departures');
-    }
-]);
-
-intents.onDefault([
-    function (session, results) {
-        session.send('Sorry, I didn\'t quite get that :/ Try Again!');
-        session.send('What do you want to do?');
-    }
-]);
+bot.dialog('/', new builder.IntentDialog()
+    .matches(/departures/i, '/departures')
+    .onDefault(builder.DialogAction.send("I'm sorry. I didn't understand. What do you want to do?"))
+);
 
 bot.dialog('/departures', [
+    function (session, args) {
+        if (args.matched.input.indexOf("from") == -1) session.beginDialog('/departures/chooseStation');
+        else {
+            str = args.matched.input + " ";
+            pos = str.indexOf("from") + 6;
+            var left = str.slice(0, pos + 1).search(/\S+$/),
+                right = str.slice(pos).search(/\s/);
+            if (right < 0) {
+                session.privateConversationData.stationName = str.slice(left);
+            }
+            session.privateConversationData.stationName = str.slice(left, right + pos);
+            session.beginDialog('/departures/confirmStation');
+        }
+    }
+]);
+
+bot.dialog('/departures/chooseStation', [
     function (session) {
         builder.Prompts.text(session, 'Ok! Which\'s stations departures do you want to see?');
     },
     function (session, results) {
-        session.dialogData.station = results.response;
-        api.getStations(results.response)
+        session.privateConversationData.stationName = results.response;
+        session.beginDialog('/departures/confirmStation');
+    }
+]);
+
+bot.dialog('/departures/confirmStation', [
+    function (session, args) {
+        api.getStations(session.privateConversationData.stationName)
           .then(response => {
             // Access the list of stops
             var stops = response['stopLocationOrCoordLocation'];
-            // Get the id of the first stop in the list (the best match)
-            session.dialogData.station = stops[0]['StopLocation']['name'];
-            session.send("I got this station: %s?", session.dialogData.station);
+            // Get the name and id of the first stop in the list (the best match)
+            session.privateConversationData.stationId = stops[0]['StopLocation']['id'];
+            session.privateConversationData.stationName = stops[0]['StopLocation']['name'];
+
+            session.send("I got this station: %s?", session.privateConversationData.stationName);
             builder.Prompts.confirm(session, "Is that right?");
             // Get departures for that stop and return a promise
             return;
-          })
+          });
     },
-    function (session, results, next) {
-        if (!results.response) session.beginDialog('/departures');
-        else next();
-    },     
     function (session, results) {
-        session.send('Ok, showing you departures from %s!', session.dialogData.station);
+        if (results.response) session.beginDialog('/departures/showResults');
+        else session.beginDialog('/departures/chooseStation');
+        session.endDialog();
+    }
+]);
 
-        api.getStations(session.dialogData.station)
+bot.dialog('/departures/showResults', [ 
+    function (session, results) {
+
+        api.getDepartures(session.privateConversationData.stationId)
           .then(response => {
-            // Access the list of stops
-            var stops = response['stopLocationOrCoordLocation'];
-            // Get the id of the first stop in the list (the best match)
-            var bestMatchId = stops[0]['StopLocation']['id'];
-            // Get departures for that stop and return a promise
-            return api.getDepartures(bestMatchId);
-          })
-          .then(response => {
+
             // Iterate list of departures
             var departures = response['Departure'];
+
+            session.send('Ok, showing you departures from %s!', session.privateConversationData.stationName);
+
             for (var departure of departures) {
               // Get time without seconds
               var time = departure.time.slice(0, 5);
@@ -68,8 +81,12 @@ bot.dialog('/departures', [
               // Print to console
               console.log(`${time} ${line} → ${departure.direction}`);
             }
-          });
 
-          session.endDialog();
+            session.endDialog();
+          })
+          .catch(error => {
+            session.send('There was an error with the api:\n %s!', error);
+            session.endDialog();
+          });
     }
 ]);
