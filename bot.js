924313d64c0c23884d7d2b6ba469515c14334b10
Willi Raschkowski <willi@raschkowski.com>
2016-10-01 02:21:18 +0200

    Remove token

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var api = require('./playground.js');
 var callbackFunc = function(body) {
   var id = JSON.parse(body).stopLocationOrCoordLocation[0].StopLocation.id;
   api.fetchData('departureBoard', {
-    accessId: "hackerstolz-01102016",
+    accessId: "",
     format: "json",
     id: id
   }, function (body) {
@@ -12,7 +12,7 @@ var callbackFunc = function(body) {
 };
 
 var data = api.fetchData('location.name', {
-  accessId: "hackerstolz-01102016",
+  accessId: "",
   format: "json",
   input: "rosenthaler",
   type: "S"
